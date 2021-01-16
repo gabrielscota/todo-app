7d28583e2c352c9aafb63f345cfb298261e6fa99
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-01-16 16:30:05 -0300

    add: LoadTaksFactory

---
+++ lib/main/factories/factories.dart
@@ -0,0 +1,2 @@
+export './cache/cache.dart';
+export './usecases/usecases.dart';
---
+++ lib/main/factories/usecases/load_tasks_factory.dart
@@ -0,0 +1,6 @@
+import '../../../data/usecases/usecases.dart';
+import '../../../domain/usecases/usecases.dart';
+import '../factories.dart';
+
+LoadTasks makeLocalLoadSurveys() =>
+    LocalLoadTasks(cacheStorage: makeLocalStorageAdapter());

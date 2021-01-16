c8b23af4efc2a46b7b2adc593738782eeead98fc
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-01-16 16:32:39 -0300

    add: SplashPageFactory

--- lib/main/factories/factories.dart
+++ lib/main/factories/factories.dart
@@ -1,2 +1,3 @@
 export './cache/cache.dart';
+export './pages/pages.dart';
 export './usecases/usecases.dart';
--- lib/main/factories/usecases/load_tasks_factory.dart
+++ lib/main/factories/usecases/load_tasks_factory.dart
@@ -2,5 +2,5 @@ import '../../../data/usecases/usecases.dart';
 import '../../../domain/usecases/usecases.dart';
 import '../factories.dart';
 
-LoadTasks makeLocalLoadSurveys() =>
+LoadTasks makeLocalLoadTasks() =>
     LocalLoadTasks(cacheStorage: makeLocalStorageAdapter());

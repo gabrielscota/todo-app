927da209edce0401b81bfa655154164bb087f530
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-01-16 14:55:30 -0300

    add: HttpAdapter

---
+++ lib/data/http/http_client.dart
@@ -0,0 +1,10 @@
+import 'package:meta/meta.dart';
+
+abstract class HttpClient {
+  Future<dynamic> request({
+    @required String url,
+    @required String method,
+    Map body,
+    Map headers,
+  });
+}
